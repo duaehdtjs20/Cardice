26eefec20e81e40f5301116db5b1bc50c0385bf5
YDS <82944267+duaehdtjs20@users.noreply.github.com>
2026-09-23 11:34:16 +0900

    faet : 전투 화면 추가

---
+++ Assets/02.Scripts/UI/Battle/BattlePanel.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+
+using UnityEngine;
+
+public class BattlePanel : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _attackerHpText;
+    [SerializeField] private TMP_Text _attackerMaxDamageText;
+    [SerializeField] private TMP_Text _attackerMinDamageText;
+
+    [SerializeField] private TMP_Text _defenderHpText;
+    [SerializeField] private TMP_Text _defenderMaxDefenseText;
+    [SerializeField] private TMP_Text _defenderMinDefenseText;
+
+    [SerializeField] private TMP_Text _attackerDiceValue;
+    [SerializeField] private TMP_Text _defenderDiceValue;
+
+    public void DrawInfo(IDamageable attacker,  IDamageable defender)
+    {
+        _attackerHpText.text = "Hp : " + attacker.Hp.ToString();
+        _attackerMaxDamageText.text = attacker.AttackDamage.ToString();
+        _attackerMinDamageText.text = attacker.AttackDamage.ToString();
+
+        _defenderHpText.text = "Hp : " + defender.Hp.ToString();
+        _defenderMaxDefenseText.text = defender.Defense.ToString();
+        _defenderMinDefenseText.text = defender.Defense.ToString();
+    }
+    public void DrawDiceValue(int attackerValue, int defenderValue)
+    {
+        _attackerDiceValue.text = attackerValue.ToString();
+        _defenderDiceValue.text = defenderValue.ToString();
+    }
+}
